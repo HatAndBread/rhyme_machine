8fde3f112f5da252175ddf8e029943a5bf4a5d49
Joshua Hume <valencelectron@hotmail.com>
2021-04-25 07:50:35 +0900

    first commit

---
+++ hashMaker.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const readWords = () => {
+  return new Promise((resolve) => {
+    const newWords = [];
+    const wordInterface = readline.createInterface({
+      input: fs.createReadStream('./words.txt')
+    });
+    wordInterface.on('line', function (line) {
+      newWords.push({ word: line, pos: null });
+    });
+    wordInterface.on('close', () => {
+      resolve(newWords);
+    });
+  });
+};
+
+const readPos = (newWords) => {
+  return new Promise((resolve) => {
+    const posInterface = readline.createInterface({
+      input: fs.createReadStream('./parts-of-speech.txt')
+    });
+    let i = 0;
+    posInterface.on('line', function (line) {
+      newWords[i].pos = line;
+      i += 1;
+    });
+    posInterface.on('close', () => {
+      resolve(newWords);
+    });
+  });
+};
+
+readWords().then((result) => {
+  readPos(result).then((result) => {
+    console.log(result);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log('Hello world!');
+
+const rhymeSet = [{ word: 'disease', pos: 'singularNoun' }];
